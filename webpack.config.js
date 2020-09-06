348608e0435fd481d368c2ddb61e0da6dbc29d99
saowang <saowang@users.noreply.github.com>
2020-09-06 12:09:14 +0000

    6b63bb82859066c3c0460772cac93abd88cbf6ec

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: {
-        theme: ["./assets/js/theme.debug.js", "./_sass/index.scss"]
+        production: ["./assets/js/development.js", "./_sass/index.scss"]
     },
     output: {
         filename: "js/[name].js",
